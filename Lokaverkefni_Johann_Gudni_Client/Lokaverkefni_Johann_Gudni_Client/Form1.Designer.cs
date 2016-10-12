f3a2742f46914a3d09ef186a8e57f278a19c4695
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2016-10-12 14:57:02 +0000

    Grunnur

---
+++ Lokaverkefni_Johann_Gudni_Client/Lokaverkefni_Johann_Gudni_Client/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Lokaverkefni_Johann_Gudni_Client
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb_output = new System.Windows.Forms.RichTextBox();
+            this.bt_guess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb_output
+            // 
+            this.rtb_output.Location = new System.Drawing.Point(12, 225);
+            this.rtb_output.Name = "rtb_output";
+            this.rtb_output.Size = new System.Drawing.Size(313, 96);
+            this.rtb_output.TabIndex = 0;
+            this.rtb_output.Text = "";
+            // 
+            // bt_guess
+            // 
+            this.bt_guess.Location = new System.Drawing.Point(117, 188);
+            this.bt_guess.Name = "bt_guess";
+            this.bt_guess.Size = new System.Drawing.Size(101, 23);
+            this.bt_guess.TabIndex = 1;
+            this.bt_guess.Text = "Submit";
+            this.bt_guess.UseVisualStyleBackColor = true;
+            this.bt_guess.Click += new System.EventHandler(this.bt_guess_Click_1);
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 333);
+            this.Controls.Add(this.bt_guess);
+            this.Controls.Add(this.rtb_output);
+            this.Name = "ClientForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_output;
+        private System.Windows.Forms.Button bt_guess;
+    }
+}
+
---
+++ Lokaverkefni_Johann_Gudni_Client/Lokaverkefni_Johann_Gudni_Client/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace Lokaverkefni_Johann_Gudni_Client
+{
+    public partial class ClientForm : Form
+    {
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+        private BinaryReader reader;
+        private BinaryWriter writer;
+        private Thread outputThread; 
+        private TcpClient connection; 
+        private NetworkStream stream;        
+        private bool done = false;
+        private TextBox tb_textGuess;
+        private RadioButton[] rd_buttonGuess;
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new TcpClient("127.0.0.1", 50000);
+                stream = connection.GetStream();
+                writer = new BinaryWriter(stream);
+                reader = new BinaryReader(stream);
+
+                outputThread = new Thread(new ThreadStart(Run));
+                outputThread.Start();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Server Is Currently Down! Please Try Again Later.");
+                Environment.Exit(0);
+            }
+
+        }
+
+        
+        private delegate void DisplayWordDelegate(string message);
+
+        private void DisplayMessage(string message)
+        {
+            if (rtb_output.InvokeRequired)
+            {
+                Invoke(new DisplayWordDelegate(DisplayMessage),
+                   new object[] { message });
+            }
+            else
+                rtb_output.Text = message;
+        }
+
+        public void ProcessMessage(string message)
+        {
+            if (message == "Win")
+            {
+                MessageBox.Show("Takk Fyrir Leikinn");
+
+
+                Environment.Exit(0);
+            }
+            else if (message.Split()[0] == "Lose")
+            {
+                MessageBox.Show("You lost, The correct word is " + message.Split()[1]);
+
+
+                Environment.Exit(0);
+
+            }
+            else
+            {
+                DisplayMessage(message);
+            }
+      
+               
+        }
+
+        public void Run()
+        {
+
+            try
+            {
+                while (!done)
+                    ProcessMessage(reader.ReadString());
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Server Is Down, Game Over!!", "Error",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+        }
+
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                done = true;
+                System.Environment.Exit(System.Environment.ExitCode);
+            }
+            catch (Exception)
+            {
+
+                Environment.Exit(0);
+            }
+            
+        }
+
+        private void bt_guess_Click_1(object sender, EventArgs e)
+        {
+            writer.Write(tb_textGuess.Text.ToLower());
+            tb_textGuess.Clear(); 
+        }
+
+    }
+}
